8683e1774ff4f0a48a567af75d73e5551a9294f5
Z8MB1E <computerboy1999@gmail.com>
2022-04-29 12:54:03 -0400

    Automate config options based on OS

--- server/build/config.js
+++ server/build/config.js
@@ -5,13 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB = void 0;
 const mysql_1 = __importDefault(require("mysql"));
-exports.DB = mysql_1.default.createPool({
+const DB_OPTIONS = process.env.ISWINDOWS ? {
     connectionLimit: 10,
     port: 3306,
     host: "localhost",
-    // socketPath: "/var/run/mysqld/mysqld.sock",
     user: "local_prod",
     password: "Production1365#",
     database: "wedding-site",
-});
+} : {
+    connectionLimit: 10,
+    socketPath: "/var/run/mysqld/mysqld.sock",
+    user: "local_prod",
+    password: "Production1365#",
+    database: "wedding-site",
+};
+exports.DB = mysql_1.default.createPool(DB_OPTIONS);
 // export const isWindows = "build";
--- server/config.ts
+++ server/config.ts
@@ -1,12 +1,19 @@
 import mysql from "mysql";
-export const DB = mysql.createPool({
+const DB_OPTIONS = process.env.ISWINDOWS ? {
   connectionLimit: 10,
   port: 3306,
   host: "localhost",
-  // socketPath: "/var/run/mysqld/mysqld.sock",
   user: "local_prod",
   password: "Production1365#",
   database: "wedding-site",
-});
+} : {
+  connectionLimit: 10,
+  socketPath: "/var/run/mysqld/mysqld.sock",
+  user: "local_prod",
+  password: "Production1365#",
+  database: "wedding-site",
+}
+
+export const DB = mysql.createPool(DB_OPTIONS);
 
 // export const isWindows = "build";
